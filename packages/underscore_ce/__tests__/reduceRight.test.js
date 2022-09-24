721f983ecf606b4a6bfd897f84fc8ff736e6b75f
gygirl <1308265464@qq.com>
2022-09-24 17:42:28 +0800

    '提交'

---
+++ packages/underscore_ce/__tests__/reduceRight.test.js
@@ -0,0 +1,54 @@
+const {
+  strictEqual, noop, keys, toArray, deepEqual
+} = require('../utils');
+const reduceRight = require('../lib/reduceRight');
+
+describe('reduceRight', () => {
+  let list = reduceRight(['foo', 'bar', 'baz'], function (memo, str) { return memo + str; }, '');
+  strictEqual(list, 'bazbarfoo', 'can perform right folds');
+
+  list = reduceRight(['foo', 'bar', 'baz'], function (memo, str) { return memo + str; });
+  strictEqual(list, 'bazbarfoo', 'default initial value');
+
+  const sum = reduceRight({ a: 1, b: 2, c: 3 }, function (memo, num) { return memo + num; });
+  strictEqual(sum, 6, 'default initial value on object');
+
+  strictEqual(reduceRight(null, noop, 138), 138, 'handles a null (with initial value) properly');
+  strictEqual(reduceRight([reduceRight], noop), reduceRight, 'collection of length one with no initial value returns the first item');
+
+  strictEqual(reduceRight([], noop, void 0), void 0, 'undefined can be passed as a special case');
+  strictEqual(reduceRight([], noop), void 0, 'returns undefined when collection is empty and no initial value');
+
+  // Assert that the correct arguments are being passed.
+
+  let args;
+  const init = {};
+  let object = { a: 1, b: 2 };
+  let lastKey = keys(object).pop();
+
+  let expected = lastKey === 'a'
+    ? [init, 1, 'a', object]
+    : [init, 2, 'b', object];
+
+  reduceRight(object, function (...opts) {
+    if (!args) args = toArray(...opts);
+  }, init);
+
+  deepEqual(args, expected);
+
+  // And again, with numeric keys.
+
+  object = { 2: 'a', 1: 'b' };
+  lastKey = keys(object).pop();
+  args = null;
+
+  expected = lastKey === '2'
+    ? [init, 'a', '2', object]
+    : [init, 'b', '1', object];
+
+  reduceRight(object, function (...opts) {
+    if (!args) args = toArray(...opts);
+  }, init);
+
+  deepEqual(args, expected);
+});
